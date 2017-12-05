ac55aa6e994844b6ad0516f3a8723f6ee4c269c9
maxlqs <maxinteger65536@gmail.com>
2017-12-06 06:25:10 +0800

    bind the templates

--- proj/impweb/views.py
+++ proj/impweb/views.py
@@ -1,6 +1,34 @@
-from django.shortcuts import render
+from django.http import HttpResponse
+from django.shortcuts import render,get_object_or_404
 
-from django.db import models
+from .models import User
+
 # Create your views here.
-class Test(models.Model):
-    name = models.CharField(max_length=20)
+'''
+def index(request):
+    return HttpResponse('Fuck Homepage')
+'''
+def register(request):
+    pass
+
+def login(request):
+    error_info='accessable, please login'
+    context={'error_info':error_info}
+    return render(request,'login.html',context)
+
+def index(request):
+    user_list=User.objects.order_by('id')[:5]
+    context={'user_list':user_list}
+    return render(request,'impweb/index.html',context)
+
+def detail(request,userid):
+    user = get_object_or_404(User, pk=userid)
+    return render(request, 'impweb/detail.html', {'user': user})
+#    return HttpResponse('The userid is %s'%userid)
+
+def results(request, question_id):
+    response = "You're looking at the results of question %s."
+    return HttpResponse(response % question_id)
+
+
+
